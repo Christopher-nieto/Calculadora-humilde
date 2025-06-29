5b73a4efb9ed1784a9e51f14e46ace0676c1b563
Christopher-nieto <Christophernieto.personal@gmail.com>
2025-06-29 14:01:15 -0500

    Add files via upload

---
+++ Calculadore.py
@@ -0,0 +1,9 @@
+print('Esta es una calculadora')
+print('Ingrese el valor a')
+A = int(input())
+print('Ingrese el valor b')
+B = int(input())
+C = A+B
+D = A-B
+print('El resultado fue de', C)
+print('Si fuera resta seria',D)
